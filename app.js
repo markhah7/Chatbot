7dbc5e0911a903dd7f547717db93ba19b62ccaad
Khoa Nguyen <khoa0702@gmail.com>
2020-02-03 00:06:20 +0700

    Add new codes

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 const express = require("express");
-const db = require("./db/db"); // Set up the express app
 const fs = require("fs");
 const bodyParser = require("body-parser");
-const app = express(); // get all todos
+const app = express();
 app.use(bodyParser.json());
+var db = [];
 
 app.get("/api/v1/todos", (req, res) => {
-  res.status(200).send({
-    success: "true",
-    message: "todos retrieved successfully",
-    todos: db
+  fs.readFile("./db/db.json", "UTF-8", (err, text) => {
+    if (err) {
+      console.log(`An error has occured:${err.message}`);
+      process.exit();
+    }
+    console.log(text);
+    db = JSON.parse(text);
+    res.status(200).send({
+      success: "true",
+      message: "todos retrieved successfully",
+      todos: db
+    });
+  });
+});
+
+app.post("/api/v1/addtodo", (req, res) => {
+  let arrayStr = [];
+  db.forEach(element => {
+    arrayStr.push(element);
+  });
+  arrayStr.push(req.body);
+  fs.writeFile("./db/db_1.json", `${JSON.stringify(arrayStr)}`, err => {
+    if (err) {
+      throw err;
+    }
+  });
+  fs.readFile("./db/db.json", "UTF-8", (err, text) => {
+    if (err) {
+      console.log(`An error has occured:${err.message}`);
+      process.exit();
+    }
+    console.log(text);
+    db = JSON.parse(text);
+    res.status(200).send({
+      success: "true",
+      message: "todos retrieved successfully",
+      todos: db
+    });
   });
 });
 
