a7cc8e284b4dea0914d543472ceee1f75b6f558d
Khoa Nguyen <khoa0702@gmail.com>
2020-02-02 13:42:54 +0700

    Add new package for parsing json data

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const db = require("./db/db"); // Set up the express app
+const fs = require("fs");
+const bodyParser = require("body-parser");
 const app = express(); // get all todos
+
+app.use(bodyParser.json());
+
 app.get("/api/v1/todos", (req, res) => {
   res.status(200).send({
     success: "true",
@@ -8,6 +13,7 @@ app.get("/api/v1/todos", (req, res) => {
     todos: db
   });
 });
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
