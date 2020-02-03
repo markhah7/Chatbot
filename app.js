931d14a1a8d5d4768c7a2adb2bcb90fc466e64c0
Khoa Nguyen <khoa0702@gmail.com>
2020-02-03 10:08:34 +0700

    Adjust logic of writing file

--- app.js
+++ app.js
@@ -27,22 +27,22 @@ app.post("/api/v1/addtodo", (req, res) => {
     arrayStr.push(element);
   });
   arrayStr.push(req.body);
-  fs.writeFile("./db/db_1.json", `${JSON.stringify(arrayStr)}`, err => {
+  fs.writeFile("./db/db.json", `${JSON.stringify(arrayStr)}`, err => {
     if (err) {
       throw err;
     }
-  });
-  fs.readFile("./db/db.json", "UTF-8", (err, text) => {
-    if (err) {
-      console.log(`An error has occured:${err.message}`);
-      process.exit();
-    }
-    console.log(text);
-    db = JSON.parse(text);
-    res.status(200).send({
-      success: "true",
-      message: "todos retrieved successfully",
-      todos: db
+    fs.readFile("./db/db.json", "UTF-8", (err, text) => {
+      if (err) {
+        console.log(`An error has occured:${err.message}`);
+        process.exit();
+      }
+      console.log(text);
+      db = JSON.parse(text);
+      res.status(200).send({
+        success: "true",
+        message: "todos retrieved successfully",
+        todos: db
+      });
     });
   });
 });
